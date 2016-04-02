eae7aa8c2ab1f6a657eefab38d65f8262f4b6a2b
rpmonteiro <ric.p.monteiro@gmail.com>
2016-04-02 21:54:38 +0100

    modified the dashboard

--- dashboard.ios.js
+++ dashboard.ios.js
@@ -25,6 +25,25 @@ let Dashboard = React.createClass({
   }
 });
 
-
+let LeaderboardCard = React.createClass({
+  render: function() {
+    return(
+      <View>
+        <Card>
+          <Card.Media
+            image={<Image source={require('./../img/welcome.jpg')} />}
+            overlay
+          />
+          <Card.Body>
+              <Text>Some text to go in the body.</Text>
+          </Card.Body>
+          <Card.Actions position="right">
+              <Button value="ACTION" />
+          </Card.Actions>
+        </Card>
+      </View>
+    )
+  }
+})
 
 export {Dashboard};
