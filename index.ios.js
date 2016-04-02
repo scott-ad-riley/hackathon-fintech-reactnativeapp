36a2eb93028beff4cbda2a26b072260d7ef08565
rpmonteiro <ric.p.monteiro@gmail.com>
2016-04-02 13:18:32 +0100

    more splash screen work

--- index.ios.js
+++ index.ios.js
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  */
 import Button from 'apsl-react-native-button'
-
+import {setTheme, MKColor, MKButton} from 'react-native-material-kit';
 import React, {
   AppRegistry,
   Component,
@@ -15,34 +15,93 @@ import React, {
 
 class saveGen extends Component {
   render() {
-    console.log(styles.welcomeButtonContainer)
     return (
       <View style={styles.windowContainer}>
         <View style={styles.mainContainer}>
-          <Image
-             style={styles.icon}
-             source={require('./assets/img/smiley.png')}
-           />
-           <Image/>
+          <WelcomeImage/>
+          <WelcomeMessage/>
         </View>
-        <View style={styles.footerContainer}>
-          <Button style={styles.welcomeButtonContainer} textStyle={[styles.welcomeButtonText]}>
-            I agree to start saving money!
-          </Button>
-        </View>
+        <FooterButton/>
       </View>
     );
   }
 }
 
+let WelcomeMessage = React.createClass({
+  render: function() {
+    return (
+      <Text style={styles.baseText}>
+        <Text style={styles.welcomeText}>
+          Welcome!
+        </Text>
+        <Text>We're here to help. But you need to promise us something</Text>
+      </Text>
+    )
+  }
+})
 
+let WelcomeImage = React.createClass({
+  render: function() {
+    return (
+    <View style={styles.iconContainter}>
+      <Image
+        style={styles.icon}
+        source={require('./assets/img/smiley.png')}
+      />
+      <Image/>
+    </View>
+    )
+  }
+})
+
+let FooterButton = React.createClass({
+  render: function() {
+    return(
+      <View style={styles.footerContainer}>
+        <MKButton
+          backgroundColor={MKColor.LightGreen}
+          width={300}
+          shadowRadius={2}
+          shadowOffset={{width:0, height:2}}
+          shadowOpacity={.7}
+          padding={10}
+          shadowColor="black"
+          onPress={() => {
+            console.log('hi, raised button!');
+          }}
+          >
+          <Text pointerEvents="none"
+                style={styles.baseText}>
+            I promise that I'll save money!
+          </Text>
+        </MKButton>
+      </View>
+    )
+  }
+})
 
 const styles = StyleSheet.create({
   windowContainer: {
     flex: 1,
     backgroundColor: '#F5FCFF',
   },
+  baseText: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: 'white',
+  },
+  welcomeText: {
+    fontFamily: 'Roboto',
+    fontSize: 25,
+    color: 'black',
+  },
+  iconContainter: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   icon: {
+    margin: 70,
+    justifyContent: 'center',
     height: 300,
     width: 300,
   },
@@ -50,14 +109,13 @@ const styles = StyleSheet.create({
     flex: 0.8,
   },
   footerContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
     flex: 0.2,
   },
   welcomeButtonText : {
     fontSize: 16,
-  },
-  welcomeButtonContainer: {
-    backgroundColor: '#36FF74',
-    margin: 25,
   },
   instructions: {
     textAlign: 'center',
