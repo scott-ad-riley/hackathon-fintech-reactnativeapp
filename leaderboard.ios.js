c1fde3880f771e912c987f445a5a2cdd66a72acc
Scott Riley <scott@imajica.com>
2016-04-03 09:16:18 +0100

    adds basic leaderboard.ios.js

---
+++ leaderboard.ios.js
@@ -0,0 +1,20 @@
+import React, {
+  Component,
+  Text,
+  StyleSheet,
+  View,
+  Image
+} from 'react-native';
+import {styles} from './styles.ios.js';
+import {setTheme, MKColor, MKButton} from 'react-native-material-kit';
+import {NextButton} from './buttons.ios.js';
+
+let Leaderboard = React.createClass({
+  render: function () {
+    return(
+      <View style={styles.windowContainer}>
+      )
+  }
+});
+
+export {Leaderboard};
